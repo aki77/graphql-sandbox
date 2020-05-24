57809ba3593aef03d480169085df7dab035e4d7b
aki <lala.akira@gmail.com>
2020-05-24 18:01:51 +0900

    部屋を指定してメッセージ取得するように

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
-    field :chat_room_messages, Types::ChatRoomMessageType.connection_type, null: false
+    field :chat_room_messages, Types::ChatRoomMessageType.connection_type, null: false do
+      description 'メッセージ一覧をpaginationで取得します'
+      argument :chat_room_id,  ID, required: true
+    end
 
-    def chat_room_messages
-      ChatRoomMessage.order(id: :desc)
+    def chat_room_messages(chat_room_id:)
+      ChatRoomMessage.where(chat_room_id: chat_room_id).order(id: :desc)
     end
   end
 end
